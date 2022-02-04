311d0a95b1a24db9d3a923891e3d70ce8792dc08
joaoggnogueira <joaogabriel17.0@hotmail.com>
2022-02-04 08:33:33 -0300

    bundle compiler

--- contract/compile.js
+++ contract/compile.js
@@ -1,17 +1,29 @@
 // compile code will go here
 const solc = require("solc")
-const fs = require("fs")
+const fs = require("fs-extra")
 const path = require("path")
 
-function compile(contract) {
-  const contractPath = path.resolve(__dirname, "contracts", contract + ".sol")
-  const source = fs.readFileSync(contractPath, "utf8")
-  const compiled = solc.compile(source, 1)
-  if (compiled.errors) {
-    console.error(...compiled.errors)
-  }
-  const contracts = compiled.contracts
-  return contracts[":" + contract]
+const contract = "Campaign"
+
+const buildPath = path.resolve(__dirname, "build")
+fs.removeSync(buildPath)
+
+const contractPath = path.resolve(__dirname, "contracts", contract + ".sol")
+const source = fs.readFileSync(contractPath, "utf8")
+const compiled = solc.compile(source, 1)
+
+fs.ensureDirSync(buildPath)
+
+for (let contract in compiled.contracts) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.substring(1) + ".json"),
+    compiled.contracts[contract]
+  )
 }
 
-module.exports = compile
+if (compiled.errors) {
+  console.error(...compiled.errors)
+}
+const contracts = compiled.contracts
+
+module.exports = contracts
